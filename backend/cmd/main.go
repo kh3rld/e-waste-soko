6e08ec0d3dee9b3c7953fcdb03b9b9b0445275a1
kh3rld <kherld.hussein@gmail.com>
2025-01-08 15:49:18 +0300

    Feat(server): Implement simple server to run the backend

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,1 +1,26 @@
-package cmd
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kh3rld/e-waste-soko/database"
+	"github.com/kh3rld/e-waste-soko/middleware"
+	"github.com/kh3rld/e-waste-soko/routes"
+)
+
+func main() {
+	app := gin.New()
+
+	database.ConnectDatabase()
+
+	routes.SetupRoutes(app.Group("/"))
+
+	app.Use(middleware.AuthMiddleware())
+
+	port := ":3000"
+	log.Printf("Server running on http://localhost%s", port)
+	if err := app.Run(port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
